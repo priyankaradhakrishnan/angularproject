ddd71d6246f4668f4290e399415b78ed504e4aa6
priyankaradhakrishnan <priyanka.radhakrishnan@gmail.com>
2017-05-09 11:41:06 +0530

    Update multiplan.component.ts

--- app/components/multiplan.component.ts
+++ app/components/multiplan.component.ts
@@ -1,7 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import {enableProdMode} from '@angular/core';
 import{ Multiplanservice } from '../services/multiplan.service';
-
+//https://plnkr.co/edit/01UjWY3TKfP6pgwXKuEa?p=preview
 @Component({
     selector:'multiplans',
     template:`<h1>Multiplan</h1><ul *ngFor="let multiplan of multiplans"><li>{{multiplan.title}}</li></ul>`
